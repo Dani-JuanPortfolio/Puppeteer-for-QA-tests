0c8f1a80696ed4fc7bef8db6b767bb5b862a51e3
Juan Pablo Nieblas Caraveo <58700956+juannieblas@users.noreply.github.com>
2020-03-16 11:57:01 -0700

    quick modiffys
    asd

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-var url = process.argv[2];
 const screenshoot = require('./modules/responsive.js');
 const readline = require("readline");
-
 const input = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
-
-
-console.log('ingresa una url')
 
 input.question('ingrese la URL: ', (answer) => {
     input.close();
@@ -16,6 +11,3 @@ input.question('ingrese la URL: ', (answer) => {
     screenshoot.functions.screenshot_tablet(answer);
     screenshoot.functions.screenshot_phone(answer);
 });
-
-
-//screenshoot.functions.screenshot_window(url)
--- modules/responsive.js
+++ modules/responsive.js
@@ -31,5 +31,4 @@ var functions = {
 
 }
 
-
 exports.functions = functions;
